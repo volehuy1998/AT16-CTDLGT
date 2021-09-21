fb069f91edf84a22d3fb1d612b1003c09012a977
VO LE HUy <volehuy1998@gmail.com>
2021-09-21 20:19:29 +0700

    Update - input from keyboard & setup output dir

--- Bao cao lan 1 - convert number/Bao cao lan 1 - convert number.cpp
+++ Bao cao lan 1 - convert number/Bao cao lan 1 - convert number.cpp
@@ -128,17 +128,27 @@ int exercise_2(std::string value, int want_from)
 
 int main()
 {
-	int input = 253;
+	int input = 0;
+	int want_to = 0;
+	std::cout << "Enter number (decimal): ";
+	std::cin >> input;
+	std::cout << "Enter number (want to convert: example 2,8,16): ";
+	std::cin >> want_to;
+	std::string bin = exercise_1(input, want_to);
+	std::cout << "Result of " << input << " to " << want_to << ": " << bin << std::endl;
 
-	std::string bin = exercise_1(input, 2);
-	std::string oct = exercise_1(input, 8);
-	std::string hex = exercise_1(input, 16);
+	std::cout << "------------------------------------------------------------------\n";
 
-	int bin_2_dec = exercise_2(bin, 2);
-	int oct_2_dec = exercise_2(oct, 8);
-	int hex_2_dec = exercise_2(hex, 16);
+	std::string input_str = "";
+	int want_from = 0;
+	std::cout << "Enter string (6F): ";
+	std::cin >> input_str;
+	std::cout << "Enter number (example: 2,8,16): ";
+	std::cin >> want_from;
 
-	std::cout << bin_2_dec << std::endl;
-	std::cout << oct_2_dec << std::endl;
-	std::cout << hex_2_dec << std::endl;
+	int decimal = exercise_2(input_str, want_from);
+	std::cout << "Result of " << input_str << " to " << want_from << ": " << decimal << std::endl;
+
+	int temp;
+	std::cin >> temp;
 }
