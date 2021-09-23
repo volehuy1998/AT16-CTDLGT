38ffac0e60ad2fb8af1366480822f2fab9f5dcd2
VO LE HUy <volehuy1998@gmail.com>
2021-09-23 12:27:56 +0700

    Improve

--- Bao cao lan 1 - convert number/Bao cao lan 1 - convert number.cpp
+++ Bao cao lan 1 - convert number/Bao cao lan 1 - convert number.cpp
@@ -2,73 +2,22 @@
 //
 
 #include <iostream>
-#include <vector>
 #include <stack>
-#include <utility> // using pair
 
-std::vector< std::pair<char, int> > rename_map =
-{
-	{'F', 15},
-	{'E', 14},
-	{'D', 13},
-	{'C', 12},
-	{'B', 11},
-	{'A', 10},
-};
+char num_to_char_map[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+int char_to_num_map[255] = { 0 };
 
-template<typename from_type, typename target_type>
-target_type get_value(from_type from_value)
-{
-	target_type result;
-
-	auto it = std::find_if(
-		rename_map.begin(),
-		rename_map.end(),
-		[from_value](const std::pair<char, int>& element) {
-			if (std::is_same<target_type, int>::value)
-			{
-				return element.first == from_value;
-			}
-			else if (std::is_same<target_type, char>::value)
-			{
-				return element.second == from_value;
-			}
-		});
-
-	if (std::is_same<target_type, int>::value)
-	{
-		if (it == rename_map.end())
-		{
-			// not found
-			result = from_value - 48;
-		}
-		else
-		{
-			// found
-			result = (*it).second;
-		}
-	}
-	else if (std::is_same<target_type, char>::value)
-	{
-		if (it == rename_map.end())
-		{
-			// not found
-			result = from_value + 48;
-		}
-		else
-		{
-			// found
-			result = (*it).first;
-		}
-	}
-
-	return result;
-}
-
-std::string convert_decimal_to_X(int N, int want_to)
+/// <summary>
+/// convert decimal to X using stack
+/// </summary>
+/// <param name="N">decimal</param>
+std::string exercise_1(int N, int want_to)
 {
 	std::stack<int> my_stack;
 	std::string result = "";
+
+	if (N < 0)
+		return "Not support yet!";
 
 	// build data stack
 	do
@@ -83,23 +32,11 @@ std::string convert_decimal_to_X(int N, int want_to)
 	{
 		int top = my_stack.top();
 		my_stack.pop();
-		char single_char = get_value<int, char>(top);
+		char single_char = num_to_char_map[top];
 		result += single_char;
 	}
 
 	return result;
-}
-
-/// <summary>
-/// convert decimal to X using stack
-/// </summary>
-/// <param name="N">decimal</param>
-std::string exercise_1(int N, int want_to)
-{
-	if (N < 0)
-		return "Not support yet!";
-
-	return convert_decimal_to_X(N, want_to);
 }
 
 /// <summary>
@@ -116,7 +53,7 @@ int exercise_2(std::string value, int want_from)
 	for (int i = 0; i < len; i++)
 	{
 		char single_char = value[i];
-		int single_decimal = get_value<char, int>(single_char);
+		int single_decimal = char_to_num_map[single_char];
 		result += single_decimal * std::pow(want_from, (len - 1) - i);
 	}
 
@@ -125,11 +62,17 @@ int exercise_2(std::string value, int want_from)
 
 int main()
 {
+	// set up
+	for (int i = 0; i < 16; i++)
+	{
+		char_to_num_map[num_to_char_map[i]] = i;
+	}
+
 	int input = 0;
 	int want_to = 0;
 	std::cout << "Enter number (decimal): ";
 	std::cin >> input;
-	std::cout << "Enter number (want to convert: example 2,8,16): ";
+	std::cout << "Enter number (convert to: example 2,8,16): ";
 	std::cin >> want_to;
 	std::string bin = exercise_1(input, want_to);
 	std::cout << "Result of " << input << " to " << want_to << ": " << bin << std::endl;
@@ -138,13 +81,13 @@ int main()
 
 	std::string input_str = "";
 	int want_from = 0;
-	std::cout << "Enter string (6F): ";
+	std::cout << "Enter string (example: hexa 6F): ";
 	std::cin >> input_str;
-	std::cout << "Enter number (example: 2,8,16): ";
+	std::cout << "Enter number (convert from: 2,8,16): ";
 	std::cin >> want_from;
 
 	int decimal = exercise_2(input_str, want_from);
-	std::cout << "Result of " << input_str << " to " << want_from << ": " << decimal << std::endl;
+	std::cout << "Result of " << input_str << " from " << want_from << " to 10:" << decimal << std::endl;
 
 	int temp;
 	std::cin >> temp;
